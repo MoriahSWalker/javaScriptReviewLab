e69c7685639aca13eda1e50bf081efc563bd4091
MoriahSWalker git config --global user.email riahrosebuds@gmail.com <you@example.com>
2022-12-01 14:46:55 -0500

    my first commit

---
+++ reviewLab.js
@@ -0,0 +1,56 @@
+/*
+A. Q + A
+How do we assign a value to a variable?  
+With the assignment operator
+
+
+How do we change the value of a variable?
+Call or Select the variable and set = "new value"
+
+How do we assign an existing variable to a new variable?
+Set existingVariable = newVariable
+
+
+Remind me, what are declare, assign, and define?
+Declare - creating a variable (var, let, const)
+Assign - giving variable a value
+Define -  when a varibale is assigned to a value it is "defined"
+
+What is pseudocoding and why should you do it?
+-psedocoding is a way to apply logic to your code before it is written
+-helps to clearly see steps you want to take to write the code
+
+What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+- 80%
+*/
+
+
+
+
+// B. Strings
+// Create a variable called firstVariable
+var firstVariable 
+
+// Assign it the value of the string "Hello World"
+var firstVariable = ("Hello World");
+
+// Change the value of this variable to some number
+var firstVariable = (32);
+
+// Store the value of firstVariablein a new variable called secondVariable
+var secondVariable = firstVariable;
+
+// Change the value of secondVariableto any string.
+var secondVariable = ("Hello");
+
+// What is the value of firstVariable? Should be "Hello" but I dont know what I'm doing wrong. 
+console.log(firstVariable) 
+
+// Create a variable called yourNameand set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourNameso that it returns a new string with them concatenated.
+var yourName = "Moriah"
+console.log(`Hello, my name is ${yourName}`)
+
+// ex: Hello, my name is Jean Valjean
+
+
+ 
